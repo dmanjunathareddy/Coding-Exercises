09ecf711a4324d77dda8d7186e1e4083a82b5b51
dmanjunathareddy <dmanjunathareddy@gmail.com>
2024-01-13 15:07:11 +0530

    CQRS desgin pattern example

---
+++ DesignPatterns/HLD/Coordinational/CQRS/OrderQueryHandler.java
@@ -0,0 +1,10 @@
+package com.softgroup.behavioral.designpatterns.cqrs;
+
+public class OrderQueryHandler {
+    Order getOrderDetails(String orderId) {
+        // Fetch order details from the read database model
+        // Return the order object
+        System.out.println("Fetching order details for order ID: " + orderId);
+        return new Order();
+    }
+}
