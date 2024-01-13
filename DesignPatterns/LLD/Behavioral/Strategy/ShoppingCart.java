98227580a6f6cb6a03311cce271f6fe525a7616c
dmanjunathareddy <dmanjunathareddy@gmail.com>
2024-01-13 10:00:30 +0530

    Strategy design pattern example

---
+++ DesignPatterns/LLD/Behavioral/Strategy/ShoppingCart.java
@@ -0,0 +1,13 @@
+package com.softgroup.behavioral.designpatterns.strategy;
+
+public class ShoppingCart {
+	private PaymentStrategy paymentStrategy;
+
+	public void setPaymentStrategy(PaymentStrategy paymentStrategy) {
+		this.paymentStrategy = paymentStrategy;
+	}
+
+	public void checkout(int amount) {
+		paymentStrategy.pay(amount);
+	}
+}
