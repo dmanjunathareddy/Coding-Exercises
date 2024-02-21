388dea176709a1e76ff914ad81922c269e116520
dmanjunathareddy <dmanjunathareddy@gmail.com>
2024-02-21 21:55:34 +0530

    Climbing stairs case problem

---
+++ DSA/20.DP/1.ClimbingStairs.java
@@ -0,0 +1,24 @@
+package com.softgroup.dsa.dp;
+
+public class ClimbingStairs {
+
+	public static int climbStairs(int n) {
+		if (n <= 2)
+			return n;
+
+		int[] dp = new int[n + 1];
+		dp[1] = 1;
+		dp[2] = 2;
+
+		for (int i = 3; i <= n; i++) {
+			dp[i] = dp[i - 1] + dp[i - 2];
+		}
+
+		return dp[n];
+	}
+
+	public static void main(String[] args) {
+		int n = 6; // Change the value of n for different test cases
+		System.out.println("Number of distinct ways to climb " + n + " stairs: " + climbStairs(n));
+	}
+}
