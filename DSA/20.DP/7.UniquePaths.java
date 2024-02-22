2ea8dc53c0603c3793c2160bf59e14fcf8f6c07e
dmanjunathareddy <dmanjunathareddy@gmail.com>
2024-02-22 21:45:54 +0530

    unique paths

---
+++ DSA/20.DP/7.UniquePaths.java
@@ -0,0 +1,26 @@
+package com.softgroup.dsa.dp;
+
+public class UniquePaths {
+
+    public static int uniquePaths(int m, int n) {
+        int[][] dp = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            dp[i][0] = 1;
+        }
+        for (int j = 0; j < n; j++) {
+            dp[0][j] = 1;
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+
+    public static void main(String[] args) {
+        int m = 3; // Number of rows
+        int n = 7; // Number of columns
+        System.out.println("Total unique paths: " + uniquePaths(m, n)); // Output: 28
+    }
+}
