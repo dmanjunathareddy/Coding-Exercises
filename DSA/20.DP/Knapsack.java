a5c3d91bdafabcb6f6c28d78eab9de625c01f44a
dmanjunathareddy <dmanjunathareddy@gmail.com>
2024-02-27 21:24:23 +0530

    Knapsack problem

---
+++ DSA/20.DP/Knapsack.java
@@ -0,0 +1,30 @@
+package com.softgroup.dsa.dp;
+
+public class Knapsack {
+
+	public static int knapsack(int W, int[] wt, int[] val, int n) {
+		int[][] dp = new int[n + 1][W + 1];
+
+		// Build table dp[][] in bottom-up manner
+		for (int i = 0; i <= n; i++) {
+			for (int w = 0; w <= W; w++) {
+				if (i == 0 || w == 0)
+					dp[i][w] = 0;
+				else if (wt[i - 1] <= w)
+					dp[i][w] = Math.max(val[i - 1] + dp[i - 1][w - wt[i - 1]], dp[i - 1][w]);
+				else
+					dp[i][w] = dp[i - 1][w];
+			}
+		}
+
+		return dp[n][W];
+	}
+
+	public static void main(String[] args) {
+		int[] val = { 60, 100, 120 };
+		int[] wt = { 10, 20, 30 };
+		int W = 50;
+		int n = val.length;
+		System.out.println("Maximum value that the thief can steal: " + knapsack(W, wt, val, n));
+	}
+}
