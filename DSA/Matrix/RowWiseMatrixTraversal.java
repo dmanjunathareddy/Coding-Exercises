df60ea28d880a712b11bba883b5f9ce48246dd73
dmanjunathareddy <dmanjunathareddy@gmail.com>
2024-03-18 20:09:24 +0530

    Row-wise traversal

---
+++ DSA/Matrix/RowWiseMatrixTraversal.java
@@ -0,0 +1,19 @@
+package com.softgroup.dsa.matrix;
+
+public class RowWiseMatrixTraversal {
+    public static void main(String[] args) {
+        int[][] matrix = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+        // Row-wise traversal
+        System.out.println("Row-wise traversal:");
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
