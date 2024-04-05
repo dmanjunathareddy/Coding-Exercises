2af2a3f448f0a0b08c7fb91043cab6028a1e5425
dmanjunathareddy <dmanjunathareddy@gmail.com>
2024-04-05 22:15:14 +0530

    Minimum jumps to end

---
+++ DSA/Arrays/MinimumJumpsToEnd.java
@@ -0,0 +1,38 @@
+package com.softgroup.dsa.array;
+
+public class MinimumJumpsToEnd {
+	public static int minJumps(int[] arr) {
+		int n = arr.length;
+		if (n <= 1)
+			return 0;
+
+		if (arr[0] == 0)
+			return -1;
+
+		int maxReach = arr[0];
+		int steps = arr[0];
+		int jumps = 1;
+
+		for (int i = 1; i < n; i++) {
+			if (i == n - 1)
+				return jumps;
+
+			maxReach = Math.max(maxReach, i + arr[i]);
+			steps--;
+
+			if (steps == 0) {
+				jumps++;
+				if (i >= maxReach)
+					return -1;
+				steps = maxReach - i;
+			}
+		}
+
+		return -1;
+	}
+
+	public static void main(String[] args) {
+		int[] arr = { 1, 3, 5, 8, 9, 2, 6, 7, 6, 8, 9 };
+		System.out.println("Minimum number of jumps: " + minJumps(arr));
+	}
+}
