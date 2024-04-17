3f48b75b3f72b689cc7f3b26e11732a24444926e
dmanjunathareddy <dmanjunathareddy@gmail.com>
2024-04-17 20:04:41 +0530

    TrappingRainWater

---
+++ DSA/TwoPointerTechnique_ProblemsAndSolutions/TrappingRainWater.java
@@ -0,0 +1,42 @@
+package com.softgroup.dsa.greedy.twopointer;
+
+public class TrappingRainWater {
+    public static int trap(int[] height) {
+        if (height == null || height.length == 0) {
+            return 0;
+        }
+
+        int left = 0;
+        int right = height.length - 1;
+        int leftMax = 0;
+        int rightMax = 0;
+        int waterTrapped = 0;
+
+        while (left <= right) {
+            if (height[left] <= height[right]) {
+                if (height[left] >= leftMax) {
+                    leftMax = height[left];
+                } else {
+                    waterTrapped += leftMax - height[left];
+                }
+                left++;
+            } else {
+                if (height[right] >= rightMax) {
+                    rightMax = height[right];
+                } else {
+                    waterTrapped += rightMax - height[right];
+                }
+                right--;
+            }
+        }
+
+        return waterTrapped;
+    }
+
+    // Example usage
+    public static void main(String[] args) {
+        int[] height = {0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1};
+        int water = trap(height);
+        System.out.println("Water trapped: " + water + " units");
+    }
+}
