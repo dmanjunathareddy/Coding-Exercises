4578b7435aec7cbd7d495dca24c8ea3851649604
dmanjunathareddy <dmanjunathareddy@gmail.com>
2024-04-24 21:41:14 +0530

    RotateImage

---
+++ DSA/Matrix/RotateImage.java
@@ -0,0 +1,46 @@
+package com.softgroup.dsa.matrix;
+
+public class RotateImage {
+
+    public static void rotate(int[][] matrix) {
+        int n = matrix.length;
+        // Transpose the matrix
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        // Reverse each row
+        for (int i = 0; i < n; i++) {
+            int start = 0;
+            int end = n - 1;
+            while (start < end) {
+                int temp = matrix[i][start];
+                matrix[i][start] = matrix[i][end];
+                matrix[i][end] = temp;
+                start++;
+                end--;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+
+        rotate(matrix);
+
+        // Print the rotated matrix
+        for (int[] row : matrix) {
+            for (int cell : row) {
+                System.out.print(cell + " ");
+            }
+            System.out.println();
+        }
+    }
+}
