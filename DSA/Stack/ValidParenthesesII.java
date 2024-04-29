693e6709662971d4fd98277c91a6701d8879bcd8
dmanjunathareddy <dmanjunathareddy@gmail.com>
2024-04-29 22:56:46 +0530

    ValidParenthesesII

---
+++ DSA/Stack/ValidParenthesesII.java
@@ -0,0 +1,41 @@
+package com.softgroup.dsa.stack;
+
+import java.util.Stack;
+
+public class ValidParenthesesII {
+    public static void main(String[] args) {
+        String s = "()[]{}";
+        ValidParenthesesII solution = new ValidParenthesesII();
+        System.out.println(solution.checkValidString(s)); // Output: true
+    }
+
+    public boolean checkValidString(String s) {
+        Stack<Integer> leftStack = new Stack<>();
+        Stack<Integer> starStack = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '(') {
+                leftStack.push(i);
+            } else if (c == '*') {
+                starStack.push(i);
+            } else {
+                if (!leftStack.isEmpty()) {
+                    leftStack.pop();
+                } else if (!starStack.isEmpty()) {
+                    starStack.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+
+        while (!leftStack.isEmpty() && !starStack.isEmpty()) {
+            if (leftStack.pop() > starStack.pop()) {
+                return false;
+            }
+        }
+
+        return leftStack.isEmpty();
+    }
+}
