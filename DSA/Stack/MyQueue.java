6ed43480c3deaf3298203e03f75207fc609e59c6
dmanjunathareddy <dmanjunathareddy@gmail.com>
2024-04-29 22:55:30 +0530

    MyQueue

---
+++ DSA/Stack/MyQueue.java
@@ -0,0 +1,48 @@
+package com.softgroup.dsa.stack;
+
+import java.util.Stack;
+
+public class MyQueue {
+    private Stack<Integer> stack1;
+    private Stack<Integer> stack2;
+
+    public MyQueue() {
+        stack1 = new Stack<>();
+        stack2 = new Stack<>();
+    }
+
+    public static void main(String[] args) {
+        MyQueue queue = new MyQueue();
+        queue.push(1);
+        queue.push(2);
+        System.out.println(queue.peek());  // Output: 1
+        System.out.println(queue.pop());   // Output: 1
+        System.out.println(queue.empty()); // Output: false
+    }
+
+    public void push(int x) {
+        stack1.push(x);
+    }
+
+    public int pop() {
+        if (stack2.isEmpty()) {
+            while (!stack1.isEmpty()) {
+                stack2.push(stack1.pop());
+            }
+        }
+        return stack2.pop();
+    }
+
+    public int peek() {
+        if (stack2.isEmpty()) {
+            while (!stack1.isEmpty()) {
+                stack2.push(stack1.pop());
+            }
+        }
+        return stack2.peek();
+    }
+
+    public boolean empty() {
+        return stack1.isEmpty() && stack2.isEmpty();
+    }
+}
