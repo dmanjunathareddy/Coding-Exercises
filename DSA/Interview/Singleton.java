97a64c1bb5e2044dad4d624cd93cbed085caa358
dmanjunathareddy <dmanjunathareddy@gmail.com>
2024-05-02 21:35:00 +0530

    Singleton

---
+++ DSA/Interview/Singleton.java
@@ -0,0 +1,20 @@
+package com.softgroup.dsa.interview;
+
+public class Singleton {
+	private static volatile Singleton instance;
+
+	private Singleton() {
+	}
+
+	public static Singleton getInstance() {
+		if (instance == null) {
+			synchronized (Singleton.class) {
+				if (instance == null) {
+					instance = new Singleton();
+				}
+			}
+		}
+		return instance;
+	}
+
+}
