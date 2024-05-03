e6b23b078084f392f9fee2b59cefba6ffabaf000
dmanjunathareddy <dmanjunathareddy@gmail.com>
2024-05-03 21:06:27 +0530

    TrappingRainWater

---
+++ DSA/String/TrappingRainWater.java
@@ -0,0 +1,35 @@
+package com.softgroup.dsa.string;
+
+public class TrappingRainWater {
+    public int trap(int[] height) {
+        if (height == null || height.length == 0) return 0;
+
+        int n = height.length;
+        int[] leftMax = new int[n];
+        int[] rightMax = new int[n];
+
+        leftMax[0] = height[0];
+        for (int i = 1; i < n; i++) {
+            leftMax[i] = Math.max(leftMax[i - 1], height[i]);
+        }
+
+        rightMax[n - 1] = height[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            rightMax[i] = Math.max(rightMax[i + 1], height[i]);
+        }
+
+        int trappedWater = 0;
+        for (int i = 0; i < n; i++) {
+            trappedWater += Math.min(leftMax[i], rightMax[i]) - height[i];
+        }
+
+        return trappedWater;
+    }
+
+    public static void main(String[] args) {
+        TrappingRainWater trappingRainWater = new TrappingRainWater();
+        int[] height = {0,1,0,2,1,0,1,3,2,1,2,1};
+        int trappedWater = trappingRainWater.trap(height);
+        System.out.println("Trapped Water: " + trappedWater);
+    }
+}
