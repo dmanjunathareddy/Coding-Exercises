d136b19d0adc131034d2f7ba5a854ba515a12799
dmanjunathareddy <dmanjunathareddy@gmail.com>
2024-05-06 19:21:03 +0530

    PalindromeCheck

---
+++ DSA/Arrays/PalindromeCheck.java
@@ -0,0 +1,24 @@
+package com.softgroup.dsa.array;
+
+public class PalindromeCheck {
+    public static boolean isPalindrome(String str) {
+        int left = 0, right = str.length() - 1;
+        
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right))
+                return false;
+            left++;
+            right--;
+        }
+        
+        return true;
+    }
+    
+    public static void main(String[] args) {
+        String str = "radar";
+        if (isPalindrome(str))
+            System.out.println(str + " is a palindrome.");
+        else
+            System.out.println(str + " is not a palindrome.");
+    }
+}
