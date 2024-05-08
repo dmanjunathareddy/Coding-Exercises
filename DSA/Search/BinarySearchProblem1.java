014b568d34ee9524746843e5daf75b2b7fcbe75c
dmanjunathareddy <dmanjunathareddy@gmail.com>
2024-05-08 19:33:56 +0530

    BinarySearch solution

---
+++ DSA/Search/BinarySearchProblem1.java
@@ -0,0 +1,26 @@
+package com.softgroup.dsa.binarysearch;
+
+public class BinarySearchProblem1 {
+	public static int binarySearch(int[] arr, int target) {
+		int left = 0;
+		int right = arr.length - 1;
+
+		while (left <= right) {
+			int mid = left + (right - left) / 2;
+			if (arr[mid] == target)
+				return mid;
+			else if (arr[mid] < target)
+				left = mid + 1;
+			else
+				right = mid - 1;
+		}
+		return -1;
+	}
+
+	public static void main(String[] args) {
+		int[] arr = { 1, 3, 5, 7, 9, 11, 13, 15 };
+		int target = 7;
+		int index = binarySearch(arr, target);
+		System.out.println("Index of " + target + " is: " + index);
+	}
+}
