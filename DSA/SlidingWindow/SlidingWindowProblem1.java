c7b96b84fa6423d2c290801061cbf22b3d9c0076
dmanjunathareddy <dmanjunathareddy@gmail.com>
2024-05-09 09:17:58 +0530

    SlidingWindow technique solutions

---
+++ DSA/SlidingWindow/SlidingWindowProblem1.java
@@ -0,0 +1,23 @@
+package com.softgroup.dsa.slidingwindowtechnique;
+
+public class SlidingWindowProblem1 {
+    public static int maxSumSubarray(int[] arr, int k) {
+        int maxSum = Integer.MIN_VALUE;
+        int currentSum = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            currentSum += arr[i];
+            if (i >= k - 1) {
+                maxSum = Math.max(maxSum, currentSum);
+                currentSum -= arr[i - (k - 1)];
+            }
+        }
+        return maxSum;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {4, 2, 1, 7, 8, 1, 2, 8, 1, 0};
+        int k = 3;
+        System.out.println("Maximum sum of subarray of size " + k + " is: " + maxSumSubarray(arr, k));
+    }
+}
