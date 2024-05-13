377aafd7ceae67815c6d5aa7e87b45ff83556b0f
dmanjunathareddy <dmanjunathareddy@gmail.com>
2024-05-13 22:32:10 +0530

    TwoSum

---
+++ DSA/Queue/TwoSum.java
@@ -0,0 +1,26 @@
+package com.softgroup.dsa.queue;
+
+import java.util.*;
+
+public class TwoSum {
+	public static int[] twoSum(int[] nums, int target) {
+		Map<Integer, Integer> map = new HashMap<>();
+
+		for (int i = 0; i < nums.length; i++) {
+			int complement = target - nums[i];
+			if (map.containsKey(complement)) {
+				return new int[] { map.get(complement), i };
+			}
+			map.put(nums[i], i);
+		}
+
+		throw new IllegalArgumentException("No two sum solution");
+	}
+
+	public static void main(String[] args) {
+		int[] nums = { 2, 7, 11, 15 };
+		int target = 9;
+		int[] result = twoSum(nums, target);
+		System.out.println("Two Sum Indices: " + Arrays.toString(result));
+	}
+}
