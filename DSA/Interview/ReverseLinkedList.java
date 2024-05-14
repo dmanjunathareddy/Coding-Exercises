1b25956ecbf1531f174b143ae4d9ea1df552b3e0
dmanjunathareddy <dmanjunathareddy@gmail.com>
2024-05-14 06:38:03 +0530

    ReverseLinkedList

---
+++ DSA/Interview/ReverseLinkedList.java
@@ -0,0 +1,48 @@
+package com.softgroup.dsa.interview;
+public class ReverseLinkedList {
+    static class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int val) {
+            this.val = val;
+            next = null;
+        }
+    }
+
+    public static ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode current = head;
+        while (current != null) {
+            ListNode nextNode = current.next;
+            current.next = prev;
+            prev = current;
+            current = nextNode;
+        }
+        return prev;
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+
+        System.out.println("Original List:");
+        printList(head);
+
+        head = reverseList(head);
+
+        System.out.println("Reversed List:");
+        printList(head);
+    }
+
+    public static void printList(ListNode head) {
+        ListNode current = head;
+        while (current != null) {
+            System.out.print(current.val + " ");
+            current = current.next;
+        }
+        System.out.println();
+    }
+}
