b9ed8a3d9805d9c2bb733c027ce4e12d5130624a
dmanjunathareddy <dmanjunathareddy@gmail.com>
2024-05-22 21:33:03 +0530

    Palindrome

---
+++ DSA/Arrays/Palindrome.java
@@ -0,0 +1,20 @@
+package com.softgroup.dsa.array;
+public class Palindrome {
+    public static boolean isPalindrome(String s) {
+        int left = 0;
+        int right = s.length() - 1;
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String s = "racecar";
+        System.out.println("Is palindrome: " + isPalindrome(s)); // Output: true
+    }
+}
