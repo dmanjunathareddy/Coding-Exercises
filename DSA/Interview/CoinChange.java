c6c859e3fe964dde735636f3eab9638304fed619
dmanjunathareddy <dmanjunathareddy@gmail.com>
2024-05-23 06:45:32 +0530

    CoinChange

---
+++ DSA/Interview/CoinChange.java
@@ -0,0 +1,24 @@
+package com.softgroup.dsa.interview;
+import java.util.Arrays;
+
+public class CoinChange {
+    public static int coinChange(int[] coins, int amount) {
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, amount + 1);
+        dp[0] = 0;
+        for (int i = 1; i <= amount; i++) {
+            for (int coin : coins) {
+                if (i - coin >= 0) {
+                    dp[i] = Math.max(dp[i], dp[i - coin] + 1);
+                }
+            }
+        }
+        return dp[amount] > amount ? -1 : dp[amount];
+    }
+
+    public static void main(String[] args) {
+        int[] coins = {1, 2, 5};
+        int amount = 11;
+        System.out.println("Fewest coins needed: " + coinChange(coins, amount)); // Output: 3
+    }
+}
