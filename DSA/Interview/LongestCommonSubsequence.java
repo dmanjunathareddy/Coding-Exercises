d476d99cb447c04f011b4872359709bed1c8d693
dmanjunathareddy <dmanjunathareddy@gmail.com>
2024-05-24 14:49:22 +0530

    LongestCommonSubsequence.java

---
+++ DSA/Interview/LongestCommonSubsequence.java
@@ -0,0 +1,22 @@
+package com.softgroup.dsa.interview;
+public class LongestCommonSubsequence {
+    public static int longestCommonSubsequence(String text1, String text2) {
+        int[][] dp = new int[text1.length() + 1][text2.length() + 1];
+        for (int i = 1; i <= text1.length(); i++) {
+            for (int j = 1; j <= text2.length(); j++) {
+                if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        return dp[text1.length()][text2.length()];
+    }
+
+    public static void main(String[] args) {
+        String text1 = "abcde";
+        String text2 = "ace";
+        System.out.println("Length of LCS: " + longestCommonSubsequence(text1, text2)); // Output: 3
+    }
+}
