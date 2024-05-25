9f73dd2609066276c968e8cd53d289e887211132
dmanjunathareddy <dmanjunathareddy@gmail.com>
2024-05-25 06:54:27 +0530

    DecodeWays

---
+++ DSA/Interview/DecodeWays.java
@@ -0,0 +1,28 @@
+package com.softgroup.dsa.interview;
+public class DecodeWays {
+    public static int numDecodings(String s) {
+        if (s == null || s.length() == 0 || s.charAt(0) == '0') {
+            return 0;
+        }
+        int n = s.length();
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            int oneDigit = Integer.parseInt(s.substring(i - 1, i));
+            int twoDigits = Integer.parseInt(s.substring(i - 2, i));
+            if (oneDigit >= 1 && oneDigit <= 9) {
+                dp[i] += dp[i - 1];
+            }
+            if (twoDigits >= 10 && twoDigits <= 26) {
+                dp[i] += dp[i - 2];
+            }
+        }
+        return dp[n];
+    }
+
+    public static void main(String[] args) {
+        String s = "226";
+        System.out.println("Number of ways to decode: " + numDecodings(s)); // Output: 3
+    }
+}
