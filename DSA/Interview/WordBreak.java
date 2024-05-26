03f1d9e6f0d7b706a19e9e0d2fc640e0fdfed102
dmanjunathareddy <dmanjunathareddy@gmail.com>
2024-05-26 20:38:02 +0530

    WordBreak

---
+++ DSA/Interview/WordBreak.java
@@ -0,0 +1,26 @@
+package com.softgroup.dsa.interview;
+import java.util.*;
+
+public class WordBreak {
+    public static boolean wordBreak(String s, List<String> wordDict) {
+        Set<String> wordSet = new HashSet<>(wordDict);
+        boolean[] dp = new boolean[s.length() + 1];
+        dp[0] = true;
+
+        for (int i = 1; i <= s.length(); i++) {
+            for (int j = 0; j < i; j++) {
+                if (dp[j] && wordSet.contains(s.substring(j, i))) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        return dp[s.length()];
+    }
+
+    public static void main(String[] args) {
+        String s = "leetcode";
+        List<String> wordDict = Arrays.asList("leet", "code");
+        System.out.println("Can the string be segmented: " + wordBreak(s, wordDict)); // Output: true
+    }
+}
