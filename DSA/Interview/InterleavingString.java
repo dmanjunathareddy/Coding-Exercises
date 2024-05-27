7da83d8b23edcf0b45f92a875c8118bcd2cb18f1
dmanjunathareddy <dmanjunathareddy@gmail.com>
2024-05-27 15:09:11 +0530

    InterleavingString

---
+++ DSA/Interview/InterleavingString.java
@@ -0,0 +1,34 @@
+package com.softgroup.dsa.interview;
+public class InterleavingString {
+    public static boolean isInterleave(String s1, String s2, String s3) {
+        int m = s1.length();
+        int n = s2.length();
+        if (m + n != s3.length()) return false;
+
+        boolean[][] dp = new boolean[m + 1][n + 1];
+        dp[0][0] = true;
+
+        for (int i = 1; i <= m; i++) {
+            dp[i][0] = dp[i - 1][0] && s1.charAt(i - 1) == s3.charAt(i - 1);
+        }
+        for (int j = 1; j <= n; j++) {
+            dp[0][j] = dp[0][j - 1] && s2.charAt(j - 1) == s3.charAt(j - 1);
+        }
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                dp[i][j] = (dp[i - 1][j] && s1.charAt(i - 1) == s3.charAt(i + j - 1)) ||
+                           (dp[i][j - 1] && s2.charAt(j - 1) == s3.charAt(i + j - 1));
+            }
+        }
+
+        return dp[m][n];
+    }
+
+    public static void main(String[] args) {
+        String s1 = "aabcc";
+        String s2 = "dbbca";
+        String s3 = "aadbbcbcac";
+        System.out.println("Is s3 an interleaving of s1 and s2: " + isInterleave(s1, s2, s3)); // Output: true
+    }
+}
