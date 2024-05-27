40d72bb85ad5799c738db0cd134e93f03d82a7d6
dmanjunathareddy <dmanjunathareddy@gmail.com>
2024-05-27 15:08:40 +0530

    PalindromePartitioningII

---
+++ DSA/Interview/PalindromePartitioningII.java
@@ -0,0 +1,26 @@
+package com.softgroup.dsa.interview;
+public class PalindromePartitioningII {
+    public static int minCut(String s) {
+        int n = s.length();
+        boolean[][] isPalindrome = new boolean[n][n];
+        int[] dp = new int[n];
+        
+        for (int i = 0; i < n; i++) {
+            int minCuts = i;
+            for (int j = 0; j <= i; j++) {
+                if (s.charAt(i) == s.charAt(j) && (i - j <= 2 || isPalindrome[j + 1][i - 1])) {
+                    isPalindrome[j][i] = true;
+                    minCuts = (j == 0) ? 0 : Math.min(minCuts, dp[j - 1] + 1);
+                }
+            }
+            dp[i] = minCuts;
+        }
+        
+        return dp[n - 1];
+    }
+
+    public static void main(String[] args) {
+        String s = "aab";
+        System.out.println("Minimum cuts needed for a palindrome partitioning: " + minCut(s)); // Output: 1
+    }
+}
