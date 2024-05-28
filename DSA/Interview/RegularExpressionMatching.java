c764f20e36235bb9ae96f9fe311b5f831fca4af3
dmanjunathareddy <dmanjunathareddy@gmail.com>
2024-05-28 21:58:00 +0530

    RegularExpressionMatching

---
+++ DSA/Interview/RegularExpressionMatching.java
@@ -0,0 +1,36 @@
+package com.softgroup.dsa.interview;
+public class RegularExpressionMatching {
+    public static boolean isMatch(String s, String p) {
+        int m = s.length();
+        int n = p.length();
+        boolean[][] dp = new boolean[m + 1][n + 1];
+        dp[0][0] = true;
+
+        for (int j = 1; j <= n; j++) {
+            if (p.charAt(j - 1) == '*') {
+                dp[0][j] = dp[0][j - 2];
+            }
+        }
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (p.charAt(j - 1) == s.charAt(i - 1) || p.charAt(j - 1) == '.') {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else if (p.charAt(j - 1) == '*') {
+                    dp[i][j] = dp[i][j - 2];
+                    if (p.charAt(j - 2) == s.charAt(i - 1) || p.charAt(j - 2) == '.') {
+                        dp[i][j] |= dp[i - 1][j];
+                    }
+                }
+            }
+        }
+
+        return dp[m][n];
+    }
+
+    public static void main(String[] args) {
+        String s = "aab";
+        String p = "c*a*b";
+        System.out.println("Regular expression matching: " + isMatch(s, p)); // Output: true
+    }
+}
