dca54bd4fd02c7cff5e064944ee970904ff1813d
dmanjunathareddy <dmanjunathareddy@gmail.com>
2024-06-05 21:28:18 +0530

    CommonCharacters

---
+++ DSA/Interview/LeetCode/MedianOfTwoSortedArrays.java
@@ -0,0 +1,57 @@
+package com.softgroup.dsa.interview.leetcode;
+public class MedianOfTwoSortedArrays {
+
+    public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        // Ensure nums1 is the smaller array
+        if (nums1.length > nums2.length) {
+            return findMedianSortedArrays(nums2, nums1);
+        }
+
+        int x = nums1.length;
+        int y = nums2.length;
+        int low = 0, high = x;
+
+        while (low <= high) {
+            int partitionX = (low + high) / 2;
+            int partitionY = (x + y + 1) / 2 - partitionX;
+
+            // If partitionX is 0 it means nothing is there on left side. Use -INF for maxLeftX
+            // If partitionX is length of input it means nothing is there on right side. Use +INF for minRightX
+            int maxLeftX = (partitionX == 0) ? Integer.MIN_VALUE : nums1[partitionX - 1];
+            int minRightX = (partitionX == x) ? Integer.MAX_VALUE : nums1[partitionX];
+
+            int maxLeftY = (partitionY == 0) ? Integer.MIN_VALUE : nums2[partitionY - 1];
+            int minRightY = (partitionY == y) ? Integer.MAX_VALUE : nums2[partitionY];
+
+            if (maxLeftX <= minRightY && maxLeftY <= minRightX) {
+                // We have partitioned array at the correct place
+                // Now get max of left elements and min of right elements to get the median in case of even length combined array size
+                // or get max of left for odd length combined array size.
+                if ((x + y) % 2 == 0) {
+                    return ((double)Math.max(maxLeftX, maxLeftY) + Math.min(minRightX, minRightY)) / 2;
+                } else {
+                    return (double)Math.max(maxLeftX, maxLeftY);
+                }
+            } else if (maxLeftX > minRightY) { // We are too far on the right side for partitionX. Go on left side.
+                high = partitionX - 1;
+            } else { // We are too far on the left side for partitionX. Go on right side.
+                low = partitionX + 1;
+            }
+        }
+
+        // Only we can come here if input arrays were not sorted. Throw in that scenario.
+        throw new IllegalArgumentException();
+    }
+
+    public static void main(String[] args) {
+        int[] nums1 = {1, 3};
+        int[] nums2 = {2};
+
+        System.out.println("The median is: " + findMedianSortedArrays(nums1, nums2)); // Output: 2.0
+
+        int[] nums3 = {1, 2};
+        int[] nums4 = {3, 4};
+
+        System.out.println("The median is: " + findMedianSortedArrays(nums3, nums4)); // Output: 2.5
+    }
+}
