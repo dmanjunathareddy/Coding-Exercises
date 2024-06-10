7c52f560a13e7a3764a9937a0f1f3dcfd3ec45a9
dmanjunathareddy <dmanjunathareddy@gmail.com>
2024-06-10 21:46:46 +0530

    NQueens

---
+++ DSA/Interview/LeetCode/NQueens.java
@@ -0,0 +1,77 @@
+package com.softgroup.dsa.interview.leetcode;
+import java.util.ArrayList;
+import java.util.List;
+
+public class NQueens {
+
+    public static List<List<String>> solveNQueens(int n) {
+        List<List<String>> result = new ArrayList<>();
+        char[][] board = new char[n][n];
+        
+        // Initialize the board with '.'
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                board[i][j] = '.';
+            }
+        }
+        
+        backtrack(result, board, 0);
+        return result;
+    }
+    
+    private static void backtrack(List<List<String>> result, char[][] board, int row) {
+        if (row == board.length) {
+            result.add(construct(board));
+            return;
+        }
+        
+        for (int col = 0; col < board.length; col++) {
+            if (isValid(board, row, col)) {
+                board[row][col] = 'Q';
+                backtrack(result, board, row + 1);
+                board[row][col] = '.'; // Backtrack
+            }
+        }
+    }
+    
+    private static boolean isValid(char[][] board, int row, int col) {
+        for (int i = 0; i < row; i++) {
+            if (board[i][col] == 'Q') {
+                return false;
+            }
+        }
+        
+        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+        
+        for (int i = row - 1, j = col + 1; i >= 0 && j < board.length; i--, j++) {
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+    
+    private static List<String> construct(char[][] board) {
+        List<String> res = new ArrayList<>();
+        for (int i = 0; i < board.length; i++) {
+            res.add(new String(board[i]));
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int n = 4;
+        List<List<String>> solutions = solveNQueens(n);
+        for (List<String> solution : solutions) {
+            for (String row : solution) {
+                System.out.println(row);
+            }
+            System.out.println();
+        }
+    }
+}
