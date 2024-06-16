9d4ebe5a530e20685cd801f37d3af797eaa690b8
dmanjunathareddy <dmanjunathareddy@gmail.com>
2024-06-16 21:29:02 +0530

    BestTimeToBuyAndSellStockIV

---
+++ DSA/Interview/LeetCode/BestTimeToBuyAndSellStockIV.java
@@ -0,0 +1,38 @@
+package com.softgroup.dsa.interview.leetcode;
+public class BestTimeToBuyAndSellStockIV {
+    public static int maxProfit(int k, int[] prices) {
+        if (prices == null || prices.length == 0 || k == 0) {
+            return 0;
+        }
+        
+        int n = prices.length;
+        
+        if (k >= n / 2) { // Simplified case for large k
+            int maxProfit = 0;
+            for (int i = 1; i < n; i++) {
+                if (prices[i] > prices[i - 1]) {
+                    maxProfit += prices[i] - prices[i - 1];
+                }
+            }
+            return maxProfit;
+        }
+        
+        int[][] dp = new int[k + 1][n];
+        
+        for (int i = 1; i <= k; i++) {
+            int maxDiff = -prices[0];
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = Math.max(dp[i][j - 1], prices[j] + maxDiff);
+                maxDiff = Math.max(maxDiff, dp[i - 1][j] - prices[j]);
+            }
+        }
+        
+        return dp[k][n - 1];
+    }
+
+    public static void main(String[] args) {
+        int k = 2;
+        int[] prices = {3, 2, 6, 5, 0, 3};
+        System.out.println("Maximum Profit: " + maxProfit(k, prices));
+    }
+}
