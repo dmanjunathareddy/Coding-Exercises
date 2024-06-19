7cf9d9bd85b9a1eb0d987029f29eed7da09da6e6
dmanjunathareddy <dmanjunathareddy@gmail.com>
2024-06-20 05:16:27 +0530

    IntegerToEnglishWords

---
+++ DSA/Interview/LeetCode/IntegerToEnglishWords.java
@@ -0,0 +1,42 @@
+package com.softgroup.dsa.interview.leetcode;
+public class IntegerToEnglishWords {
+    private static final String[] LESS_THAN_20 = {"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
+    private static final String[] TENS = {"", "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
+    private static final String[] THOUSANDS = {"", "Thousand", "Million", "Billion"};
+
+    public static void main(String[] args) {
+        int num = 1234567891; // Example input
+        System.out.println(numberToWords(num)); // Output: "One Billion Two Hundred Thirty Four Million Five Hundred Sixty Seven Thousand Eight Hundred Ninety One"
+    }
+
+    public static String numberToWords(int num) {
+        if (num == 0) {
+            return "Zero";
+        }
+
+        int i = 0;
+        String words = "";
+
+        while (num > 0) {
+            if (num % 1000 != 0) {
+                words = helper(num % 1000) + THOUSANDS[i] + " " + words;
+            }
+            num /= 1000;
+            i++;
+        }
+
+        return words.trim();
+    }
+
+    private static String helper(int num) {
+        if (num == 0) {
+            return "";
+        } else if (num < 20) {
+            return LESS_THAN_20[num] + " ";
+        } else if (num < 100) {
+            return TENS[num / 10] + " " + helper(num % 10);
+        } else {
+            return LESS_THAN_20[num / 100] + " Hundred " + helper(num % 100);
+        }
+    }
+}
