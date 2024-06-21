223087db97766eb1469ab69a8d47c94f9598b4d7
dmanjunathareddy <dmanjunathareddy@gmail.com>
2024-06-21 20:11:52 +0530

    MedianFinder

---
+++ DSA/Interview/LeetCode/MedianFinder.java
@@ -0,0 +1,49 @@
+package com.softgroup.dsa.interview.leetcode;
+import java.util.PriorityQueue;
+import java.util.Collections;
+
+public class MedianFinder {
+    private PriorityQueue<Integer> maxHeap; // max-heap for the lower half
+    private PriorityQueue<Integer> minHeap; // min-heap for the upper half
+
+    /** Initialize your data structure here. */
+    public MedianFinder() {
+        maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+        minHeap = new PriorityQueue<>();
+    }
+
+    /** Adds a number into the data structure. */
+    public void addNum(int num) {
+        if (maxHeap.isEmpty() || num <= maxHeap.peek()) {
+            maxHeap.offer(num);
+        } else {
+            minHeap.offer(num);
+        }
+
+        // Balance the heaps
+        if (maxHeap.size() > minHeap.size() + 1) {
+            minHeap.offer(maxHeap.poll());
+        } else if (minHeap.size() > maxHeap.size()) {
+            maxHeap.offer(minHeap.poll());
+        }
+    }
+
+    /** Returns the median of current data stream */
+    public double findMedian() {
+        if (maxHeap.size() == minHeap.size()) {
+            return (maxHeap.peek() + minHeap.peek()) / 2.0;
+        } else {
+            return maxHeap.peek();
+        }
+    }
+
+    public static void main(String[] args) {
+        MedianFinder medianFinder = new MedianFinder();
+        medianFinder.addNum(1);
+        System.out.println(medianFinder.findMedian()); // Output: 1.0
+        medianFinder.addNum(2);
+        System.out.println(medianFinder.findMedian()); // Output: 1.5
+        medianFinder.addNum(3);
+        System.out.println(medianFinder.findMedian()); // Output: 2.0
+    }
+}
