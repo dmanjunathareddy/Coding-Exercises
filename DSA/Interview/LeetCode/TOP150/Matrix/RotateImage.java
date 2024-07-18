d81e81d55620c450abfdeb499e0e5ca3b171d75a
dmanjunathareddy <dmanjunathareddy@gmail.com>
2024-07-18 16:19:25 +0530

    RotateImage

---
+++ DSA/Interview/LeetCode/TOP150/Matrix/RotateImage.java
@@ -0,0 +1,44 @@
+package com.softgroup.dsa.interview.leetcode.top150;
+public class RotateImage {
+
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+
+        // Transpose the matrix
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        // Reverse each row
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n / 2; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][n - j - 1];
+                matrix[i][n - j - 1] = temp;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        RotateImage ri = new RotateImage();
+        int[][] matrix = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+
+        ri.rotate(matrix);
+
+        // Print the rotated matrix
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
