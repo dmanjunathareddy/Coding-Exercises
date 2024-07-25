5e49927c480d0d25e4cfb8d7b4e4d3cbde17e562
dmanjunathareddy <dmanjunathareddy@gmail.com>
2024-07-25 20:53:43 +0530

    ValidSubarrays

---
+++ DSA/Interview/LeetCode/ValidSubarrays.java
@@ -0,0 +1,25 @@
+package com.softgroup.dsa.interview.leetcode;
+public class ValidSubarrays {
+    public int validSubarrays(int[] nums) {
+        int count = 0;
+        int n = nums.length;
+        int[] stack = new int[n];
+        int top = -1;
+
+        for (int i = n - 1; i >= 0; i--) {
+            while (top >= 0 && nums[i] <= nums[stack[top]]) {
+                top--;
+            }
+            count += (top == -1) ? n - i : stack[top] - i;
+            stack[++top] = i;
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+        ValidSubarrays solution = new ValidSubarrays();
+        int[] nums = {1, 4, 2, 5, 3};
+        System.out.println(solution.validSubarrays(nums)); // Output: 11
+    }
+}
