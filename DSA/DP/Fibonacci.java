d1678a6e02e92ec02bab71e634cf7c2d80240501
dmanjunathareddy <dmanjunathareddy@gmail.com>
2024-08-04 13:47:26 +0530

    Fibonacci

---
+++ DSA/DP/Fibonacci.java
@@ -0,0 +1,18 @@
+package com.softgroup.dsa.dp;
+public class Fibonacci {
+    public static int fib(int n) {
+        if (n <= 1) return n;
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
+    }
+
+    public static void main(String[] args) {
+        int n = 10; // Example
+        System.out.println("Fibonacci number at position " + n + " is: " + fib(n));
+    }
+}
